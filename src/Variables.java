cc33a20e9eb2afa169a733c3f251a186de44ab0a
bharista123 <attakpahdavid53@gmail.com>
2026-06-23 12:25:39 -0700

    this is a trial work in week 4

---
+++ src/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+
+        System.out.println("**********");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("**********");
+
+        System.out.println("     *    ");
+        System.out.println("    ***   ");
+        System.out.println("   *****  ");
+        System.out.println("  ******* ");
+        System.out.println(" *********");
+    }
+}
